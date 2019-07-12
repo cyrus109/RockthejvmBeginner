c8ec997e9e0f7aa264ee727fb5e5d5d60138b426
master <cyrus109@gmail.com>
2019-07-12 13:28:28 +0200

    add variable lessons

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,11 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x=42
+  println(x)
+  val aString: String ="Hello"
+  val aBoolean: Boolean=false
+  var toto= 41
+  toto=x
+  println(x)
+}
